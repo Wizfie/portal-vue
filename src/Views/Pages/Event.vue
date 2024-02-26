0b8c1bea93412a0235dadb7a10e7996372e0faf5
Wisfie <wiz.fie@gmail.com>
2024-02-26 17:06:12 +0700

    add team

--- src/Views/Pages/Event.vue
+++ src/Views/Pages/Event.vue
@@ -4,13 +4,16 @@
 	import { onMounted, ref } from "vue";
 	import SidebarWithNavbar from "../../components/SidebarWithNavbar.vue";
 
-	let token = "Bearer " + localStorage.getItem("authToken");
+	const token = "Bearer " + localStorage.getItem("authToken");
 	const role = ref("");
+	const username = ref("");
 
 	// Variable
-	let eventName = ref("");
-	let eventYear = ref("");
-	let steps = ref([]);
+	const eventName = ref("");
+	const eventYear = ref("");
+	const steps = ref([]);
+	const events = ref([]);
+	const eventId = ref("");
 
 	// Fungsi untuk menambahkan langkah-langkah baru
 	const addStep = () => {
@@ -26,7 +29,10 @@
 		if (userData) {
 			const parseJson = JSON.parse(userData);
 			role.value = parseJson.role;
+			username.value = parseJson.username;
 		}
+
+		getEvents();
 		initFlowbite();
 	});
 
@@ -71,7 +77,7 @@
 				to: "wizfiee@gmail.com",
 				from: "wizfie@outlook.com",
 				subject: "Event Baru",
-				name: "Kadal",
+				name: username,
 				text: `Event ${eventName.value} Sudah Buka Pendaftarannya silahkan cek di Web `,
 			};
 
@@ -88,6 +94,14 @@
 			console.error("Error:", error);
 			// Handle error
 		}
+	};
+
+	const getEvents = () => {
+		axios.get("/event/get-event").then((response) => {
+			console.log(response.data);
+			events.value = response.data.map((item) => item.event);
+			console.log(events.value);
+		});
 	};
 </script>
 
@@ -295,6 +309,159 @@
 										</ul>
 									</div> -->
 
+									<button
+										type="submit"
+										class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+									>
+										Save
+									</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div
+				class="p-4 border-2 border-gray-200 border-opacity-100 rounded-lg dark:border-gray-700 mt-5"
+			>
+				<div>
+					<!-- Modal toggle -->
+					<button
+						data-modal-target="add-team-member-modal"
+						data-modal-toggle="add-team-member-modal"
+						class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						type="button"
+					>
+						Register Event
+					</button>
+
+					<!-- Main modal -->
+					<div
+						data-modal-backdrop="static"
+						id="add-team-member-modal"
+						tabindex="1"
+						aria-hidden="true"
+						class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+					>
+						<div class="relative p-4 w-full max-w-md max-h-full">
+							<!-- Modal content -->
+							<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+								<!-- Modal header -->
+								<div
+									class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
+								>
+									<h3
+										class="text-lg font-semibold text-gray-900 dark:text-white"
+									>
+										Add New Team Member
+									</h3>
+									<button
+										type="button"
+										class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+										data-modal-toggle="add-team-member-modal"
+									>
+										<svg
+											class="w-3 h-3"
+											aria-hidden="true"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 14 14"
+										>
+											<path
+												stroke="currentColor"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+											/>
+										</svg>
+										<span class="sr-only">Close modal</span>
+									</button>
+								</div>
+								<!-- Modal body -->
+								<form @submit.prevent="addMemberTeam" class="p-4 md:p-5">
+									<div class="grid gap-4 mb-4 grid-cols-2">
+										<div class="col-span-2">
+											<label
+												for="countries"
+												class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+												>Select a Team</label
+											>
+											<select
+												id="countries"
+												required
+												v-model="idTeam"
+												class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+											>
+												<option disabled value="Select Award"></option>
+												<option
+													v-for="team in filteredTeam"
+													:key="team.id"
+													:value="team.id"
+												>
+													{{ team.nameTeam }}
+												</option>
+											</select>
+										</div>
+										<div class="col-span-2">
+											<label
+												for="countries"
+												class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+												>Select an Event</label
+											>
+											<select
+												id="countries"
+												required
+												v-model="eventId"
+												class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+											>
+												<option disabled value="Select Award"></option>
+												<option
+													v-for="event in events"
+													:key="event.eventId"
+													:value="eventId"
+												>
+													{{ event.eventName }}
+												</option>
+											</select>
+										</div>
+										<div class="col-span-2">
+											<label
+												for="name-member"
+												class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+												>Member Name
+											</label>
+											<input
+												v-model="memberName"
+												@input="memberName = $event.target.value.toUpperCase()"
+												type="text"
+												name="name-member"
+												id="name-member"
+												class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+												placeholder="Member Name"
+												required
+											/>
+										</div>
+										<div class="col-span-2">
+											<label
+												for="position-member"
+												class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+												>Position
+											</label>
+											<input
+												v-model="memberPosition"
+												@input="
+													memberPosition = $event.target.value.toUpperCase()
+												"
+												type="text"
+												name="position-member"
+												id="position-member"
+												class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+												placeholder="Position"
+												required
+											/>
+										</div>
+									</div>
 									<button
 										type="submit"
 										class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- src/Views/Pages/Teams.vue
+++ src/Views/Pages/Teams.vue
@@ -4,86 +4,41 @@
 	import { useStore } from "vuex";
 	import SidebarWithNavbarVue from "../../components/SidebarWithNavbar.vue";
 
-	let awards = ref([]);
-	let teams = ref([]);
-	let nameTeam = ref("");
-	let idAward = ref(null);
-	let idTeam = ref(null);
-	let memberName = ref("");
-	let memberPosition = ref("");
-	let teamWithMembers = ref([]);
+	const teamName = ref("");
+	const description = ref("");
+	const events = ref([]);
 	const userData = ref("");
 	const store = useStore();
 	onMounted(() => {
 		userData.value = store.getters.getUserData;
-		getAwarding();
-		getTeamWithMembers();
+		getEvent();
 	});
 
-	const filteredTeam = computed(() => {
-		console.log("Teams:", teams.value);
-		console.log("User Data:", userData.value);
-		return teamWithMembers.value.filter(
-			(team) => team.idUser == userData.value.id
-		);
-	});
-
-	const getAwarding = async () => {
-		axios.get("/get-all-awards").then((response) => {
-			awards.value = response.data;
+	const getEvent = async () => {
+		axios.get("/event/get-event").then((response) => {
+			events.value = response.data.map((item) => item.event);
+			console.log(events.value);
 		});
 	};
 
 	const createTeam = async () => {
+		const now = new Date();
 		const teamData = {
-			nameTeam: nameTeam.value,
-			idUser: userData.value.id,
+			teamName: teamName.value,
+			userId: userData.value.id,
+			description: description.value.trim(),
+			createdAt: now.toISOString(),
 		};
+
 		try {
-			// console.log(teamData);
-			axios.post("/team/create", teamData).then((response) => {
-				nameTeam.value = "";
-				alert("OK");
-				getTeam();
-			});
+			const response = await axios.post("/team/create", teamData);
+			console.log(response.data);
+			alert("Team : " + teamName.value + " Created");
+			teamName.value = "";
+			description.value = "";
 		} catch (error) {
 			console.log("error " + error);
-			alert("Error");
-		}
-	};
-
-	const addMemberTeam = async () => {
-		const memberData = {
-			nameMember: memberName.value,
-			position: memberPosition.value,
-			idTeam: idTeam.value,
-			idAwards: idAward.value,
-		};
-
-		try {
-			// console.log(memberData);
-			axios.post("/member/add", memberData).then((response) => {
-				console.log(response.data);
-
-				alert("Succes Added ");
-				memberName.value = "";
-				memberPosition.value = "";
-
-				getTeamWithMembers();
-			});
-		} catch (error) {
-			console.error("Error add member " + error);
-		}
-	};
-
-	const getTeamWithMembers = async () => {
-		try {
-			await axios.get("team/with-members").then((response) => {
-				console.log(response.data);
-				teamWithMembers.value = response.data;
-			});
-		} catch (error) {
-			console.error("error get Team with member" + error);
+			alert(error.response.data);
 		}
 	};
 </script>
@@ -168,9 +123,9 @@
 															>Team Name
 														</label>
 														<input
-															v-model="nameTeam"
+															v-model="teamName"
 															@input="
-																nameTeam = $event.target.value.toUpperCase()
+																teamName = $event.target.value.toUpperCase()
 															"
 															type="text"
 															name="name-event"
@@ -180,157 +135,19 @@
 															required
 														/>
 													</div>
-												</div>
-												<button
-													type="submit"
-													class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-												>
-													Save
-												</button>
-											</form>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div>
-								<!-- Modal toggle -->
-								<button
-									data-modal-target="add-team-member-modal"
-									data-modal-toggle="add-team-member-modal"
-									class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-									type="button"
-								>
-									Add Member Team
-								</button>
-
-								<!-- Main modal -->
-								<div
-									data-modal-backdrop="static"
-									id="add-team-member-modal"
-									tabindex="1"
-									aria-hidden="true"
-									class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-								>
-									<div class="relative p-4 w-full max-w-md max-h-full">
-										<!-- Modal content -->
-										<div
-											class="relative bg-white rounded-lg shadow dark:bg-gray-700"
-										>
-											<!-- Modal header -->
-											<div
-												class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
-											>
-												<h3
-													class="text-lg font-semibold text-gray-900 dark:text-white"
-												>
-													Add New Team Member
-												</h3>
-												<button
-													type="button"
-													class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-													data-modal-toggle="add-team-member-modal"
-												>
-													<svg
-														class="w-3 h-3"
-														aria-hidden="true"
-														xmlns="http://www.w3.org/2000/svg"
-														fill="none"
-														viewBox="0 0 14 14"
-													>
-														<path
-															stroke="currentColor"
-															stroke-linecap="round"
-															stroke-linejoin="round"
-															stroke-width="2"
-															d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-														/>
-													</svg>
-													<span class="sr-only">Close modal</span>
-												</button>
-											</div>
-											<!-- Modal body -->
-											<form @submit.prevent="addMemberTeam" class="p-4 md:p-5">
-												<div class="grid gap-4 mb-4 grid-cols-2">
 													<div class="col-span-2">
 														<label
-															for="countries"
+															for="name-event"
 															class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-															>Select a Team</label
-														>
-														<select
-															id="countries"
-															required
-															v-model="idTeam"
-															class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-														>
-															<option disabled value="Select Award"></option>
-															<option
-																v-for="team in filteredTeam"
-																:key="team.id"
-																:value="team.id"
-															>
-																{{ team.nameTeam }}
-															</option>
-														</select>
-													</div>
-													<div class="col-span-2">
-														<label
-															for="countries"
-															class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-															>Select an Awarding</label
-														>
-														<select
-															id="countries"
-															required
-															v-model="idAward"
-															class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-														>
-															<option disabled value="Select Award"></option>
-															<option
-																v-for="award in awards"
-																:key="award.id"
-																:value="award.id"
-															>
-																{{ award.nameAward }}
-															</option>
-														</select>
-													</div>
-													<div class="col-span-2">
-														<label
-															for="name-member"
-															class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-															>Member Name
+															>Description
 														</label>
-														<input
-															v-model="memberName"
-															@input="
-																memberName = $event.target.value.toUpperCase()
-															"
+														<textarea
+															v-model="description"
 															type="text"
-															name="name-member"
-															id="name-member"
+															name="name-event"
+															id="name-event"
 															class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-															placeholder="Member Name"
-															required
-														/>
-													</div>
-													<div class="col-span-2">
-														<label
-															for="position-member"
-															class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-															>Position
-														</label>
-														<input
-															v-model="memberPosition"
-															@input="
-																memberPosition =
-																	$event.target.value.toUpperCase()
-															"
-															type="text"
-															name="position-member"
-															id="position-member"
-															class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-															placeholder="Position"
+															placeholder="Desctiption"
 															required
 														/>
 													</div>
